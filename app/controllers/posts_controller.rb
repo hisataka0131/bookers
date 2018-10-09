34778baf1cd3e545f9bae782109a9ca3556b535b
石原比紗隆 <biy0ganba@gmail.com>
2018-10-09 19:45:56 +0900

    アプリケーション基礎

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,54 @@
+class PostsController < ApplicationController
+  def top
+  	
+  end
+
+  def show
+  	@post = Post.find(params[:id])
+  end
+
+  def edit
+  	@post = Post.find(params[:id])
+  end
+
+  def update
+  	post = Post.find(params[:id])
+  	if post.update(posts_parames)
+  		flash[:notice] = "Book was successfully updated"
+  	redirect_to post_path(post.id)
+  end
+  end
+
+  def index
+  	@post = Post.new
+  	@posts = Post.all.order(created_at: :desc)
+
+
+
+  end
+
+  def create
+  	post = Post.new(posts_parames)
+  	if post.save
+  		flash[:notice] = "Book was successfully created"
+  	redirect_to post_path(post.id)
+
+
+    end
+
+  end
+
+  def destroy
+  	post = Post.find(params[:id])
+  	if post.destroy
+  		flash[:notice] = "Book was successfully destroyed"
+  	redirect_to posts_path
+    end
+  end
+
+  private
+  def posts_parames
+  	params.require(:post).permit(:title, :body)
+  end
+
+end
